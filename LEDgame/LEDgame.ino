65559b51b7f4d47afaeee51d1224e81c11ea6e49
Hartvik Line <hartvik.line@gmail.com>
2015-03-17 00:01:51 +0100

    First edit

---
+++ LEDgame/LEDgame.ino
@@ -0,0 +1,26 @@
+int game;
+byte leds[8][8];
+void setup() {
+
+}
+
+
+
+void loop()
+{
+ChooseGame();
+while(1)
+  {
+  
+  
+  }  
+
+
+ChooseGame()
+  {
+   //ReadJoistick 
+  }
+ int ReadJoistick(){
+   //1=left 2=right 3=up 4=down
+   }
+}
